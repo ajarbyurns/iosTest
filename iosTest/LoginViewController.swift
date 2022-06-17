2ee0d5115fb6584f7c40afe62fbeff8b39be6d6f
datindo-Barry <barryjuans@datindo.co.id>
2022-06-17 13:56:39 +0700

    First Commit

---
+++ iosTest/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  iosTest
+//
+//  Created by bitocto_Barry on 17/06/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
